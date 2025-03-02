35a0956d1cd42ba7802aa78993f92834c34218b9
shourov.foisal <shourov.foisal@technonext.com>
2025-03-02 10:36:01 +0600

    init project

---
+++ src/main/java/com/shourov/serialization_and_deserialization/SerializationAndDeserializationApplication.java
@@ -0,0 +1,13 @@
+package com.shourov.serialization_and_deserialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerializationAndDeserializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerializationAndDeserializationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shourov/serialization_and_deserialization/SerializationAndDeserializationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shourov.serialization_and_deserialization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerializationAndDeserializationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
